67fed7ccc78a905b90a7cc4d916103a096995ba4
anshumanthakur786 <anshumansinghthakur98@gmail.com>
2025-07-09 16:27:31 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,13 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # To allow connection from React frontend
+CORS(app)
 
+# Home Page
 @app.route('/')
-def home():
-    return render_template("index.html")  # Loads UI page from templates folder
+def homepage():
+    return render_template('homepage.html')
+
+# Login Page
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# Signup Page
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+# Dashboard Page
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+# Budget Page
+@app.route('/budget')
+def budget():
+    return render_template('budget.html')
+
+# Chat Page
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
